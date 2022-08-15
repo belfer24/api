6a2a534440c6cd1a8635322f1367e7325fe41eb9
Atteroot <yuora556@gmail.com>
2022-08-15 12:53:08 +0300

    rename variable

--- src/constants/serverLinks.ts
+++ src/constants/serverLinks.ts
@@ -1,8 +1,8 @@
-const ngrokLink = 'https://charge-api-oydrvkbt6q-uc.a.run.app';
+const apiLink = 'https://charge-api-oydrvkbt6q-uc.a.run.app';
 
 const serverLinks = {
-  outlookRedirectLink: `${ngrokLink}/auth/redirect`,
-  cloudTasksRedirectLink: `${ngrokLink}/mails/send-mails`,
+  outlookRedirectLink: `${apiLink}/auth/redirect`,
+  cloudTasksRedirectLink: `${apiLink}/mails/send-mails`,
 };
 
 export default serverLinks;
